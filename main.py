82e6d4f1e23864f9ecceb96d610a871da61022c0
ctrl-alt-del <jiyang881230@gmail.com>
2014-11-30 23:20:56 -0800

    add getImportLines() method;

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 #! /usr/bin/python
 import os
+
 def getPackageLine(path):
 	with open(path) as infile:
 	    for line in infile:
 	        if "package" in line:
 	        	return line.lstrip("package").strip().rstrip(";") + "." + os.path.basename(path).rstrip(".java;")
 
+def getImportLines(path):
+	with open(path) as infile:
+	    for line in infile:
+	        if "import" in line:
+	        	mLine = line.lstrip("import").strip().rstrip(";")
+	        	
+	        	if mLine in x:
+	        		print "--> " + mLine
+	        		
+	        elif "{" in line:
+	        	break
 
 def getAllJavaFiles(path):
 	resultList = []
