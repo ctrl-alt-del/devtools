9dda88054d57a65363ed25e0cf0484055842ff2c
ctrl-alt-del <jiyang881230@gmail.com>
2014-11-30 23:17:14 -0800

    add getAllJavaFiles() method;

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 #! /usr/bin/python
+import os
+
+def getAllJavaFiles(path):
+	resultList = []
+	for name in os.listdir(path):
+
+		if name != "src" and "src" not in path:
+			continue
+
+		if os.path.isdir(path+"/"+name):
+			for each in getAllJavaFiles(path+"/"+name):
+				resultList.append(each)
+
+		if ".java" in name:
+			resultList.append(path+"/"+name)
+
+	return resultList
