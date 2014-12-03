4c43fb960dd181beae47f4811728c7c8747b652c
ctrl-alt-del <jiyang881230@gmail.com>
2014-12-03 00:05:13 -0800

    add print_class() method to JavaClass;

--- JavaClass.py
+++ JavaClass.py
@@ -1,3 +1,15 @@
 class JavaClass(object):
     def __init__(self, path):
         self.path = path
+
+    def print_class(self):
+        if self.is_interface:
+            print '(Interface) ' + self.path
+            return
+        else:
+            print '\-> ' + self.path
+
+        for subline in self.import_files:
+            print " +----> " + subline
+
+        print ''
